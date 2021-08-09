8667b303426d1d0f0996b02d26f02d987a27553b
kslsVaishnavi <88546971+kslsVaishnavi@users.noreply.github.com>
2021-08-09 18:21:16 +0530

    Add files via upload

---
+++ Aarray.java
@@ -0,0 +1,23 @@
+package aarray;
+
+import java.util.ArrayList;
+import java.util.List;
+ 	public class Aarray {
+		public static void main(String[] args){
+			List lst = new ArrayList<>(2);
+			 lst.add(0);
+			 lst.add(1);
+			 lst.add(2);
+			 lst.add(1);
+			 lst.add(4);
+			 lst.add(5);
+			 //for(int i=0;i<lst.size();i++){
+			 System.out.println(lst.get(3));
+			 //}
+			 System.out.println("size :" +lst.size());
+			
+		}
+
+	}
+
+
